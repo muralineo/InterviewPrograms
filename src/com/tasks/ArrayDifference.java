b5b6e68ba55bfa4f9615c64598f0bac0f002dbdb
murali <murali4neo@gmail.com>
2021-09-01 15:46:38 +0530

    Interview Programs

--- src/com/tasks/ArrayDifference.java
+++ src/com/tasks/ArrayDifference.java
@@ -8,7 +8,7 @@ public class ArrayDifference {
 
     public static List<String> pairList = new LinkedList<>();
 
-    public static List addPairs(int num1, int num2){
+    public static List<String> addPairs(int num1, int num2){
 
         // This conditions removes the duplicate pairs
         if (!pairList.contains("(" + num1 + "," + num2 + ")")) {
@@ -18,7 +18,7 @@ public class ArrayDifference {
         return pairList;
     }
 
-    public static List addPairsWithoutSwapping(int num1, int num2) {
+    public static List<String> addPairsWithoutSwapping(int num1, int num2) {
 
         if (!pairList.contains("(" + num1 + "," + num2 + ")") && !pairList.contains("(" + num2 + "," + num1 + ")")) {
 
@@ -29,7 +29,7 @@ public class ArrayDifference {
     }
 
 
-    public static List pairs (int[] arr, int difference) {
+    public static List<String> pairs (int[] arr, int difference, boolean isSwapping) {
         List<String> result = null;
 
         for (int i = 0; i < arr.length; i++) {
@@ -39,9 +39,11 @@ public class ArrayDifference {
                 // Checks, if the numbers are not same and difference of each number with other numbers
                 if ((j != i) && (arr[i]-arr[j] == difference || arr[i]-arr[j] == -difference  )) {
                     //Adding the pairs with given difference into a list
-//                  result = addPairs(arr[i], arr[j]);
-                    result = addPairsWithoutSwapping(arr[i], arr[j]);
-
+                    if (isSwapping) {
+                        result = addPairs(arr[i], arr[j]);
+                    } else {
+                        result = addPairsWithoutSwapping(arr[i], arr[j]);
+                    }
                 }
             }
         }
@@ -49,13 +51,11 @@ public class ArrayDifference {
         return result;
     }
 
-
-
     public static void main(String[] args) {
 
         int[] a = {1,2,2,3,5,4,2,6};
         int[] b = {2,4,-2,-4,3,-3,5,9,15};
-        System.out.println(pairs(b, 1));
+        System.out.println(pairs(b, 1, false));
 
     }
 }
